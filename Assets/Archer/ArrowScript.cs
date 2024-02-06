577ed92ad25b378980ca62bf7bc672dc797763fd
Pedro Meruge <a100709@alunos.uminho.pt>
2024-02-06 02:07:45 +0000

    Arrow doesnt collide player unless hit ground

--- Assets/Archer/ArrowScript.cs
+++ Assets/Archer/ArrowScript.cs
@@ -8,12 +8,16 @@ public class ArrowScript : MonoBehaviour
 {
     //[SerializeField] private float depth = 0.3f; //quanto a seta se deve deslocar para dentro do objeto, quando colide (usar?)
     [SerializeField] private Animator animator;
+
+    [SerializeField] private LayerMask newForceSendLayers; // updated ForceSendLayers after ground, so user collides with arrow
     public int damage = 0;
     private bool hit = false; // atingiu algum objeto na sua trajetoria
     private bool selected = false; // indica se player tem o foco de pickup nesta seta
     private SpEntity owner; // pointer para o player que criou esta seta, para n o atingir
 
     private Rigidbody2D rb;
+    private Collider2D coll;
+
 
     private void Awake()
     {
@@ -22,6 +26,7 @@ public class ArrowScript : MonoBehaviour
 
     private void Start() {
         this.rb = GetComponent<Rigidbody2D>();
+        this.coll = GetComponent<Collider2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -35,7 +40,7 @@ public class ArrowScript : MonoBehaviour
                 animator.SetBool("Stuck", true);
                 this.hit = true;
             }
-            else if (objectHit is SpEntity) // usando pool de setas mais tarde deve ser mais f�cil evitar usar isto
+            else if (objectHit is SpEntity) // usando pool de setas mais tarde deve ser mais fácil evitar usar isto
             {
                 Debug.Log("Hit player!");
                 objectHit.GetComponentInChildren<Stats>().damage(this.damage);
@@ -53,7 +58,8 @@ public class ArrowScript : MonoBehaviour
     private void ArrowStick(Collision2D obj)
     {
         //this.transform.parent = obj.transform; // faz a seta filha do outro objeto, para se mover com ele (no caso de inimigos?)
-        rb.bodyType = RigidbodyType2D.Static;
+        this.rb.bodyType = RigidbodyType2D.Static;
+        this.coll.forceSendLayers = this.newForceSendLayers;
     }
 
     //indica que est� selecionada para ser a seta pegada
