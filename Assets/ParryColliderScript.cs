7e372bd8305f876858c23730cc042751ddc039e0
Pedro Meruge <a100709@alunos.uminho.pt>
2024-02-10 16:13:08 +0000

    Parry ability for tempOffensive objects

---
+++ Assets/ParryColliderScript.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryColliderScript : MonoBehaviour
+{
+    [SerializeField] private float parryTime = 0.5f;
+    private Animator animator; // n�o usado
+    private Transform playerTransform;
+    private bool isParrying = false;
+    private List<Collider2D> currentColliders = new List<Collider2D>(); // lista de itens a colidir com zona de parry neste momento
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        SpEntity player = SEntity.getObjRoot<SpEntity>(this.gameObject);
+        playerTransform = player.GetComponentInChildren<ICollisions>().transform;
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (!currentColliders.Contains(other)) {currentColliders.Add(other);}
+        if ((isParrying) == true) {
+            ParryLogic();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        currentColliders.Remove(other);
+    }
+
+    public void Parry() {
+        if (!isParrying) {
+            StartCoroutine(ParryRoutine());
+        }
+    }
+
+    public IEnumerator ParryRoutine() {
+        isParrying = true;
+        yield return new WaitForSeconds(parryTime);
+        isParrying = false;
+    }
+    // Afeta todos os players em contacto com a �rea de ataque do player (�rea circular)
+    // D� dano e exer�e for�as contrarias
+    public void ParryLogic()
+    {
+        //Para cada objeto atingido
+        foreach (Collider2D hitObj in currentColliders)
+        {
+            SEntity entity = SEntity.getObjRoot<SEntity>(hitObj.gameObject);
+
+            if (entity is null) Debug.LogError("entity hit has null root");
+            if (entity is StoEntity) {
+                Debug.Log("Parried entity");
+                Rigidbody2D rb = hitObj.GetComponentInChildren<Rigidbody2D>();
+                int facingRight = playerTransform.localScale.x > 0 ? 1 : -1; // seta para a esq. ou dir.
+                float arrowSpeed = Mathf.Max(Mathf.Abs(rb.velocity.x),Mathf.Abs(rb.velocity.y));
+
+                rb.velocity = new Vector2(1 * Mathf.Sign(playerTransform.localScale.x), 0) * arrowSpeed * 1.2f; // decidi aumentar o speed no rebound
+                float angleInDegrees = Mathf.Acos(facingRight) * Mathf.Rad2Deg;
+                rb.transform.rotation = Quaternion.Euler(0,0, angleInDegrees);
+            }
+        }
+    }
+}
--- Assets/ParryScript.cs
+++ Assets/ParryScript.cs
@@ -4,44 +4,19 @@ using UnityEngine;
 
 public class ParryScript : MonoBehaviour
 {
-    [SerializeField] [Range (0.1f,1f)] private float k_AttackX = .2f;
-    [SerializeField] [Range (0.1f,1f)] private float k_AttackY = .2f;
-    [SerializeField] private Vector2 k_attackForce = Vector2.zero; // for�a exercida sobre objetos
-    [SerializeField] private Transform m_AttackPos; // pos. central do ataque
-    [SerializeField] private LayerMask m_WhatIsParryable; // layers com que o parry pode interagir
+    private ParryColliderScript parryScript;
 
-    private Animator animator; // n�o usado
-    private Transform m_playerPos; // saber orienta��o do player
-
-    void Start()
+    void Awake()
     {
-        animator = this.GetComponent<Animator>();
         SpEntity player = SEntity.getObjRoot<SpEntity>(this.gameObject);
         if (player == null) Debug.LogError("player root is null");
-        this.m_playerPos = player.GetComponentInChildren<ICollisions>().transform; 
+        this.parryScript = player.GetComponentInChildren<ParryColliderScript>();
     }
     
     // Afeta todos os players em contacto com a �rea de ataque do player (�rea circular)
     // D� dano e exer�e for�as contrarias
     public void Parry()
     {
-        //melhor usar OverlapBox ??? overlapBox tem formato de output em array??
-        Collider2D[] hitObjsColl = Physics2D.OverlapBoxAll(this.m_AttackPos.position, new Vector2(k_AttackX,k_AttackY),0f,m_WhatIsParryable);
-
-        Debug.DrawLine(this.m_AttackPos.position - (Vector3.right * (this.k_AttackX/2)), this.m_AttackPos.position + (Vector3.right * (this.k_AttackX/2)),Color.red,1f,false);
-        Debug.DrawLine(this.m_AttackPos.position - (Vector3.up * (this.k_AttackY/2)), this.m_AttackPos.position + (Vector3.up * (this.k_AttackY/2)),Color.red,1f,false);
-        //Para cada objeto atingido
-        foreach (Collider2D hitObj in hitObjsColl)
-        {
-            SEntity entity = SEntity.getObjRoot<SEntity>(hitObj.gameObject);
-
-            if (entity is null) Debug.LogError("entity hit has null root");
-            if (entity is StoEntity) {
-                Rigidbody2D rb = hitObj.GetComponentInChildren<Rigidbody2D>();
-                Vector2 appliedForce = rb.velocity;
-                appliedForce.x *= this.m_playerPos.localScale.x > 0 ? 1 : -1; // orientar o sentido da for�a para esq. ou dir.
-                rb.AddForce(appliedForce);
-            }
-        }
+        parryScript.Parry();
     }
 }
